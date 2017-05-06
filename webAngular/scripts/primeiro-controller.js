4e583d0223d24bdc6569db3c90f9c015bc0a658e
betinasomer <betina.somer@gmail.com>
2017-05-06 15:50:02 -0300

    Exercicio AngularJS
    Exercicio do curso AngularJS do Timtec

---
+++ webAngular/scripts/primeiro-controller.js
@@ -0,0 +1,6 @@
+angular.module('ModeloProcessoSoftware').controller('PrimeiroController', function($scope){
+	$scope.nome = 'Betina Somer';
+	var nome = "Daniel Krug";
+	
+	$scope.iniciado = false;
+});
