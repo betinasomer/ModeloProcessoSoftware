8ceb9591f004b28f40877ac5d4b14134732b492e
betinasomer <betina.somer@gmail.com>
2017-05-22 13:40:18 -0300

    Arquivo antigo
    Controller antigo, o atual esta em controllers

--- Projeto2/client/scripts/modelo-view-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('FerramentaProcesso').controller('ModeloViewController',function($scope, ModelosCollectionService) {
-    $scope.nome = 'Teste';
-    $scope.descricao = 'Exemplo';
-    $scope.codigo = 0;
-    $scope.modelos = ModelosCollectionService.getModelos();
-
-    
-});
